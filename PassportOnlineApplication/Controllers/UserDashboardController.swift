8947dffc5b5523d5090298e85976a75efc55f7cb
BlertOsmani <85968611+BlertOsmani@users.noreply.github.com>
2024-02-28 09:56:52 +0100

    User dashboard UI
    Created the user dashboard where the user will apply for the passport

--- PassportOnlineApplication/Controllers/UserDashboardController.swift
+++ PassportOnlineApplication/Controllers/UserDashboardController.swift
@@ -9,9 +9,12 @@ import Foundation
 import UIKit
 class UserDashboardController: UIViewController {
 
+    @IBOutlet weak var userNameSurname: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Utilities.setupAttributedText(to: userNameSurname)
+        
     }
 
 
